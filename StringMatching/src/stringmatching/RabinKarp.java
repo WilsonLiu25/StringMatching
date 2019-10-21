fa06639225b0a625ff76affdc3fda9352c54dc61
WilsonLiu25 <32662781+WilsonLiu25@users.noreply.github.com>
2019-10-21 17:31:28 +1300

    Working implementation of the algorithm

--- StringMatching/src/stringmatching/RabinKarp.java
+++ StringMatching/src/stringmatching/RabinKarp.java
@@ -14,13 +14,67 @@ public class RabinKarp {
     public String pattern;
     public int hash_string;
     public int hash_pattern;
+    public int length_string;
     
-    RabinKarp(String userString, String userPattern){
-        this.string = userString;
-        this.pattern = userPattern;
+    
+    public RabinKarp(){
+        
     }
     
-    public int calculateHash() {
+    public void RabinKarpSearch(String userString, String userPattern, int mod) {
+        int stringLength = userString.length();
+        int patternLength = userPattern.length();
+        int stringHash = 0;
+        int patternHash = 0;
+        int h = 1;
+        int maxChar = 256; //the MAX number of characters in the input alphabet 
+        
+        int i; //used for the for loops
+        int j; //used for the for loops
+        
+        //Calculate h value
+        for (i = 0; i < patternLength - 1; i++) {
+            h = (h * maxChar) % mod;
+        }
+        
+        //Calculate the hash value for the pattern and first characters of the string 
+        //according to patternLength value
+        for (i = 0; i < patternLength; i++) {
+            patternHash = (maxChar * patternHash + userPattern.charAt(i)) % mod;
+            stringHash = (maxChar * stringHash + userString.charAt(i)) % mod;
+        }
+        
+        //start sliding along the userString one by one
+        for (i = 0; i <= (stringLength - patternLength); i++) {
+            //check for same hash values in the current 'window' of text (same length as pattern)
+            //If they match, start checking for characters one by one
+            if (patternHash == stringHash) {
+                //check for characters individual
+                for (j = 0; j < patternLength; j++) {
+                    if (userString.charAt(i + j) != userPattern.charAt(j)) {
+                        break;
+                    }
+                }
+                
+                //if patternHash == stringHash
+                if (j == patternLength) {
+                    System.out.println("- There has been an instance of the pattern found at index " + i);
+                }
+            }
+            
+            //Continues searching by calculating hash value of next 'window' of text
+            //also - removing the last digit (as it has been checked) and add a digit to the front
+            if (i < stringLength - patternLength) {
+                //sets the new string hash for the new window of text
+                stringHash = (maxChar * (stringHash - userString.charAt(i) * h) 
+                        + userString.charAt(i + patternLength)) % mod; 
+                
+                //if somehow stringHash becomes a negative number, convert it to a positive
+                if (stringHash < 0) {
+                    stringHash = (stringHash + mod);
+                }
+            }
+        }
         
     }
         
--- StringMatching/src/stringmatching/StringMatching.java
+++ StringMatching/src/stringmatching/StringMatching.java
@@ -5,20 +5,43 @@
  */
 package stringmatching;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author will2
  */
 public class StringMatching {
+    public String userString;
+    public String userPattern;
+    public Scanner scan;
+    int mod = 101; // a prime number
+    RabinKarp rabinKarp = new RabinKarp();
+    
+    StringMatching(){
+        System.out.println(">Please enter a string you would like to get checked for patterns");
+        scan = new Scanner(System.in);
+        userString = scan.nextLine();
 
-    /**
-     * @param args the command line arguments
-     */
+        System.out.println(">Please enter a string pattern you would like to search for");
+        scan = new Scanner(System.in);
+        userPattern = scan.nextLine();
+        
+        System.out.println(">You have choosen the string " + userString 
+                + " and searching for pattern of " + userPattern);
+        
+        rabinKarp.RabinKarpSearch(userString, userPattern, mod);
+        
+    }
+    
     public static void main(String[] args) {
         System.out.println("This is an implementation of the Rabin-Karp algorithm. "
                 + "It is a string pattern matching algorithm that finds if and how many times a "
-                + "pattern is found in the given String. This is done through the use of hashing"
+                + "\npattern is found in the given String. This is done through the use of hashing"
                 + "technique and brute force comparisons.");
+        
+        StringMatching run = new StringMatching();
     }
     
 }
